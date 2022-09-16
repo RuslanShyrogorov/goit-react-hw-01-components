8f6137b8468e3c419711c83ed8b1b073646dd8fb
RuslanShyrogorov <ruslanshirogorov@gmail.com>
2022-09-17 00:15:50 +0300

    finish jsx-files: profile, statistics. Start friendlist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,12 @@
+// import { Statistics } from './Statistics/Statistics';
+// import data from 'data.json';
+// ======================================
+// import { Profile } from './Profile/Profile';
+// import user from 'user.json';
+// ======================================
+import { FriendList } from './FriendList/FriendList';
+import friends from 'friends.json';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +16,12 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      {/* <Profile data={user} /> */}
+      {/* <Statistics stats={data} title="Upload stats" /> */}
+      <FriendList friends={friends} />
     </div>
   );
 };
